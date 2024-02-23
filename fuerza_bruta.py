86ed6d520a4eb64ee21641e044810939b88ffe54
sconejera <sebcon@gmail.com>
2024-02-22 22:14:50 -0300

    primer commit

---
+++ fuerza_bruta.py
@@ -0,0 +1,23 @@
+import getpass
+from string import ascii_lowercase as abecedario
+
+intentos = 0
+todas_encontradas = 1
+password = getpass.getpass('Ingresa contraseña: ')
+
+for letra_password in password:
+    encontrado = 1
+    for letra_abecedario in abecedario:
+        intentos += 1
+        if letra_password == letra_abecedario:
+            encontrado = 1
+            break
+    if not encontrado:
+        todas_encontradas = 0
+        break
+
+if todas_encontradas:
+    print(f'Se adivinó la palabra en {intentos} intentos.')
+    print(f'La contraseña es: {password}')
+else:
+    print(f'No se pudo adivinar la contraseña.')
