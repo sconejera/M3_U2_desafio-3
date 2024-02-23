6db9a05c5fcec8ac297f43b577bf20980834ae15
sconejera <sebcon@gmail.com>
2024-02-23 00:41:27 -0300

    actualizacion primeros_auxilios

--- primeros_auxilios.py
+++ primeros_auxilios.py
@@ -1,3 +1,31 @@
-prueba
 
+while True:
+    responde_estimulos = input("¿Responde a estímulos? (s/n): ")
+    if responde_estimulos == 's':
+        print("Valorar la necesidad de llevar al hospital más cercano")
+        print("Fin")
+        
+    else:
+        print("Abrir la vía aérea")
+        respira = input("¿Respira? (s/n): ")
+        if respira == 's':
+            print("Permitirle posición de suficiente ventilación")
+            print("Fin")            
+        else:
+            print("Administrar 5 Ventilaciones y llamar a Ambulancia")
+            signos_de_vida = input("¿Signos de Vida? (s/n): ")
+            while signos_de_vida == 'n':
+                print("Administrar Compresiones Torácicas hasta que llegue ambulancia")
+                llego_ambulancia = input("¿Llegó la Ambulancia? (s/n): ")
+                if llego_ambulancia == 's':
+                    print("Fin")
+                    break                  
+                else:
+                    signos_de_vida = input("¿Signos de Vida? (s/n): ")
+            if signos_de_vida == 's':
+                print("Reevaluar a la espera de la Ambulancia")
+                llego_ambulancia = input("¿Llegó la Ambulancia? (s/n): ")
+                if llego_ambulancia == 's':
+                    print("Fin")
+    break
 
